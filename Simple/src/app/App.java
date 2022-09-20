d24e2ba6a5c8dd623267b9ff71678860b70e12b1
vaanavi <nithi.nimme@gmail.com>
2022-09-20 19:01:21 -0400

    Hello Nithya welcome

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -3,6 +3,6 @@ package app;
  class App{
 
     public static void main(String[] args){
-        System.out.println("Hello Nithya !!!!");
+        System.out.println("Hello Nithya welcome !!!!");
     }
 }
